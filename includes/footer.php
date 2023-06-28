317a055a3e8fff90e8288467425dafb68105ea45
Syed Usama <syedusama875@gmail.com>
2023-06-28 03:10:10 -0700

    footer added

--- includes/footer.php
+++ includes/footer.php
@@ -1,28 +1,69 @@
-<footer>
+<div class="footer-fashion">
+	<div class="fashion-div">
+		<div class="col-md-12">
+			<a href="#">Fashable is arround the world<i class="fas fa-arrow-right fashion-icon"></i></a>
+		</div>
+	</div>
+</div>
+<footer class="main-footer">
    <div class="footer-div">
    	<div class="row">
-   		<div class="col-md-4">
+   		<div class="col-md-5">
    			<div class="widget">
-   				
+   				<img src="assets/images/footer-logo.png" alt="">
+				<p class="footer-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
    			</div>
    		</div>
-   		<div class="col-md-4">
+   		<div class="col-md-2">
    			<div class="widget">
-   				
+   				<h2 class="footer-h2">Quick Links</h2>
+                	<ul class="footer-menu">
+                        <li class="active"><a href="./">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                	</ul>
    			</div>
    		</div>
-   		<div class="col-md-4">
+   		<div class="col-md-2">
    			<div class="widget">
-   				
+			   <h2 class="footer-h2">Shop</h2>
+                	<ul class="footer-menu">
+                        <li class="active"><a href="./">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                	</ul>
+   			</div>
+   		</div>
+		   <div class="col-md-3">
+   			<div class="widget">
+			   <h2 class="footer-h2">Newsletter</h2>
+			   <form action="">
+				<input type="text" placeholder="Type your E-mail Here">
+				<button class="form-btn">Send</button>
+			   </form>
+			   <i class="fab fa-facebook foot-icon"></i>
+			   <i class="fab fa-twitter foot-icon"></i>
+			   <i class="fab fa-youtube foot-icon"></i>
+			   <i class="fab fa-instagram foot-icon"></i>
    			</div>
    		</div>
    	</div>
      
    </div>
 </footer>
-
+<div class="bottom-btn">
+	<div class="bottom-btn-div">
+		<div class="col-md-12 text-right">
+		<i class="fas fa-arrow-circle-up bot-btn"></i>
+		</div>
+	</div>
+</div>
  <div class="copyright text-center">
  	<div class="container">
-         <p>All Rights Reserved</p>
+         <p>Copywrite 2023, All Rights Reserved Rad</p>
          </div>
   </div>
